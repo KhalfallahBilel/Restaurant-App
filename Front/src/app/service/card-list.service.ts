67875c06bcff016f25c2a53b301b75a63dacf14b
khalfallah bilel <khalfallah.bilel@gmail.com>
2018-09-03 08:41:50 +0200

    ok

--- Front/src/app/service/card-list.service.ts
+++ Front/src/app/service/card-list.service.ts
@@ -2,10 +2,6 @@ import { Injectable } from '@angular/core';
 import { CardList } from '../model/cardList';
 
 
-const MOCKCARD = [
-  new CardList('./assets/img-plats/1.jpg','Nuggets','Voyage Organisé'),
-  new CardList('./assets/img-plats/2.jpg','Burger','Voyage Organisé'),
-  new CardList('./assets/img-plats/3.jpg','Mlawi','Voyage Organisé')
   
 ];
 @Injectable({
